6636f718967cc89c806d8c16b8954aef6cb02990
Karim Latiris <latiriskarim7@gmail.com>
2025-04-22 16:00:37 +0200

    v3 tailwind needed for ang17 v4 not supported

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+  darkMode: 'selector',
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
